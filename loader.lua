6ce50a1a31485f0ae3146267279ccbfabf7c5cbf
AllGameRM112 <88487716+AllGameRM112@users.noreply.github.com>
2023-04-20 14:45:09 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,10 @@
-if game.PlaceId == 3101667897 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/legendofspeed.lua"))()
-elseif game.PlaceId == 12825009182 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/sakonnaheeeee.lua"))()
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 3101667897 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/legendofspeed.lua'))()
+    warn('Execute success')
+elseif PlaceId == 12825009182 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/sakonnaheeeee.lua'))()
 else
-    game.Players.LocalPlayer:Kick('No game support naheee')
+    game.Players.LocalPlayer:Kick('Not Support this map.')
 end
