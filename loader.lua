d56595eb50934b9a4065d63f42b8fb66b941f2ba
AllGameRM112 <88487716+AllGameRM112@users.noreply.github.com>
2023-04-20 14:40:15 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 3101667897 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/legendofspeed.lua"))()
+elseif game.PlaceId == 12825009182 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/sakonnaheeeee.lua"))()
+else
+    game.Players.LocalPlayer:Kick('No game support naheee')
+end
