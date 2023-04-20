5ed60b890e86da8c4f29d752afda4070d6db857d
Sixnumz <115585031+Sixnumz@users.noreply.github.com>
2023-04-20 14:59:37 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 repeat wait() until game:IsLoaded()
-local PlaceId = game.PlaceId
-if PlaceId == 3101667897 then
+local PlaceID = game.PlaceId
+if PlaceID == 3101667897 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/legendofspeed.lua'))()
     warn('Execute success')
-elseif PlaceId == 12825009182 then
+elseif PlaceID == 12825009182 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/AllGameRM-Group/flinthub/main/sakonnaheeeee.lua'))()
 else
     game.Players.LocalPlayer:Kick('Not Support this map.')
